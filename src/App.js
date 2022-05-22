15865be6d5a82b5afcb838261dc2854e5ce0b313
Jasurbek <jasurisoqov685@gmail.com>
2022-05-22 06:56:20 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Components.js/Header";
+
+import "./Style.css";
 
 const App = () => {
-  return <div>Application</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
--- src/Components.js/Header.js
+++ src/Components.js/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
+// import { BrowserRouter, Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Cards</li>
-          <li>Contact</li>
+      <nav className="navbar">
+        <ul className="un_list">
+          <li className="link">Home</li>
+          <li className="link">About</li>
+          <li className="link">Cards</li>
+          <li className="link">Contact</li>
         </ul>
       </nav>
     </div>
