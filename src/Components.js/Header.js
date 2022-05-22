d3153332af3a5cf9004cc86c96e2604fa2ddac83
Jasurbek <jasurisoqov685@gmail.com>
2022-05-22 06:36:30 +0500

    create component and header

---
+++ src/Components.js/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Cards</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
