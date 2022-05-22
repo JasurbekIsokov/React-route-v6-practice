26e720ab334f213fd35eee6636eb03bf286eee00
Jasurbek <jasurisoqov685@gmail.com>
2022-05-22 07:59:08 +0500

    home and contact

---
+++ src/Components.js/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="container">
+      <div className="page">
+        <h1>Contact</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/Components.js/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="page">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
